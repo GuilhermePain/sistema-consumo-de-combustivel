ed940a849e08dd6022d6cbe325f41c5864c53013
Oliver.dev <oliveiramaicon16637@gmail.com>
2025-10-06 10:16:57 -0300

    feat: init variables for main file

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main () {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    double VM = 90.0;
+    double KM_L = 12.0;
+    double QTD_1 = 10.0;
+    double S = 187.0;
+    double VLR_COMB = 6.00;
+    double VLR_ABST = 0.0;
+    double TANK_CAP = 50.0;
+
+    return 0;
+}
